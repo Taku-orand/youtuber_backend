c864a8b353c6e81e487ae958084b3d8040ec1cb7
Takuya <uoastudent182@gmail.com>
2021-12-05 18:52:13 +0900

    ユーザーモデル作成
    ユーザー作成動作確認

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,25 @@ class Api::V1::UsersController < ApplicationController
       user: "たくや"
     }
   end
+
+  def create
+    new_user = User.new(users_params)
+    begin
+      new_user.save!
+      render json: {
+        created: true,
+        isLogin: true,
+      }
+    rescue => exception
+      render json: { 
+        created: false,
+        isLogin: false
+      }
+    end
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:name, :password, :email, :introduction)
+  end
 end
